1129d1fe3887661dfc94ef1f908d85524634bad6
Rahime Yildiz <rahime1yildiz@gmail.com>
2022-09-19 22:01:48 +0200

    added the solution of assessment2

--- assessment2/main.ts
+++ assessment2/main.ts
@@ -1,6 +1,20 @@
-function sumOfMultiple(limit: any): any {
-    // Your code goes here
-    return 0;
+function sumOfMultiple(limit: number): number {
+  // Your code goes here
+
+  let sum: number = 0;
+
+  for (let i = 0; i < limit; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
+    }
+  }
+  console.log("sum " + sum);
+
+  return sum;
 }
+
+const result: number = sumOfMultiple(1000);
+
+console.log(result);
 
 module.exports = sumOfMultiple;
